28a54e1b6565ea5b0028e97aa3bc76403c44e623
simon <simon@LAPTOP-N10NICR5.lan>
2021-06-08 00:03:33 +0200

    Ultimo commit

--- Recap/src/main/java/PackageNuovo/TestClassCommit.java
+++ Recap/src/main/java/PackageNuovo/TestClassCommit.java
@@ -4,10 +4,8 @@ public class TestClassCommit {
   
   public int variabileprova=0;
   public int variabile2=1;
-<<<<<<< HEAD
   public int variabile4=3;
-=======
   public int variabile3=0; //Bella!
->>>>>>> branch 'master' of https://github.com/Paglia29/TestProject.git
+
 
 }
